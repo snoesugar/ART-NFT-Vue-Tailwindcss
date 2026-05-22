576525ed911a7f26e1fc81680625b530fd59bf65
happy <daisy831015@gmail.com>
2026-05-22 16:22:37 +0800

    功能：新增 vue-masonry-css 套件，並在 Home 頁面中展示藝術品，改善顯示效果

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -313,11 +313,52 @@
         <span class="text-xl font-bold leading-none">最新藝術品</span>
         <Button class="ml-auto -mb-4" :isPrimaryBg="true" :hasBorder="false">MORE</Button>
       </div>
+      <div class="pt-12 pb-20">
+        <div class="columns-1 md:columns-2 lg:columns-4 gap-6">
+          <div
+            v-for="item in artworks"
+            :key="item.id"
+            class="break-inside-avoid mb-6 cursor-pointer"
+          >
+            <div class="relative bg-white p-4 border border-gray-200 shadow-sm overflow-hidden">
+              <img
+                :src="item.imgUrl"
+                :alt="item.title"
+                class="w-full min-h-63.5 h-auto object-cover"
+              />
+
+              <div
+                class="absolute inset-0 bg-black/70 opacity-0 hover:opacity-100 transition duration-300 flex flex-col justify-between p-6 m-4"
+              >
+                <div class="border border-white p-6 h-full flex flex-col">
+                  <div class="text-white">
+                    <p class="mb-4 whitespace-pre-line">
+                      {{ item.description }}
+                    </p>
+
+                    <div class="flex gap-2">
+                      <i class="fa-brands fa-ethereum"></i>
+                      {{ item.price }}
+                    </div>
+                  </div>
+
+                  <Button class="mt-auto ml-auto" />
+                </div>
+              </div>
+            </div>
+
+            <div class="mt-4 border-b border-black font-bold pb-4">
+              {{ item.title }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Button from '@/components/Button.vue'
 
 import { Swiper, SwiperSlide } from 'swiper/vue'
@@ -366,4 +407,65 @@ const artists = [
     desc: '專注於微觀視角，將花草的局部紋理與細胞結構轉譯為超現實的魔幻風格。',
   },
 ]
+
+// 模擬資料，故意給予不同的圖片高度或比例
+const artworks = ref([
+  {
+    id: 1,
+    title: '區塊戀-他的私生活',
+    price: 300,
+    description: '唯一想擁有的人，\n唯一不可以擁有的。',
+    imgUrl: new URL('/art09.jpg', import.meta.url).href,
+  },
+  {
+    id: 2,
+    title: '不哭不哭眼淚是珍珠',
+    price: 200,
+    description: '如果悲傷可以變現，\n我早已是這座城市最富有的人。',
+    imgUrl: new URL('/art12.jpg', import.meta.url).href,
+  },
+  {
+    id: 3,
+    title: '夏娃的醜聞',
+    price: 300,
+    description: '禁果不是她摘的，\n但罪名往往留給最美的人。',
+    imgUrl: new URL('/art10.jpg', import.meta.url).href,
+  },
+  {
+    id: 4,
+    title: '十分鐘就到',
+    price: 200,
+    description: '這句話跟「下次約吃飯」一樣，\n都是當代最大的謊言。',
+    imgUrl: new URL('/art03.jpg', import.meta.url).href,
+  },
+  {
+    id: 5,
+    title: '藍色啤酒海',
+    price: 150,
+    description: '把憂鬱倒進杯子裡，\n今晚我們在泡沫裡滅頂。',
+    imgUrl: new URL('/art02.jpg', import.meta.url).href,
+  },
+  {
+    id: 6,
+    title: '我這樣有美嗎',
+    price: 200,
+    description: '在被世界觀看之前，\n我早就對鏡子彩排了無數次。',
+    imgUrl: new URL('/art04.jpg', import.meta.url).href,
+  },
+  {
+    id: 7,
+    title: '沒有人愛我',
+    price: 500,
+    description: '孤獨是神話，\n也是現代靈魂唯一的奢侈品。',
+    imgUrl: new URL('/art11.jpg', import.meta.url).href,
+  },
+
+  {
+    id: 8,
+    title: '我真的受傷了',
+    price: 200,
+    description: '不是心碎的聲音，\n是期待再次被摔碎的鈍痛。',
+    imgUrl: new URL('/art27.jpg', import.meta.url).href,
+  },
+])
 </script>
