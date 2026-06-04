2ca45f3fd0782f98b860c443a88ab452a7bbdff3
happy <daisy831015@gmail.com>
2026-06-04 10:16:39 +0800

    功能：重構 server.js，使用解構賦值導入 json-server 模組以簡化代碼

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
-import jsonServer from 'json-server'
-import path from 'path'
+import { create, router as _router, defaults } from 'json-server'
+import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-// 在 ESM 模組中，必須這樣動態模擬出 __dirname
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const __dirname = dirname(__filename)
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const server = create()
+const router = _router(join(__dirname, 'db.json'))
+const middlewares = defaults()
 
 const port = process.env.PORT || 3000
 
